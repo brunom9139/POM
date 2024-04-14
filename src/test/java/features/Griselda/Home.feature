5a4ba5d454d84940b912f1207d06e201e78e3735
Griselda <griseldamedrano1@gmail.com>
2024-04-13 23:35:06 -0300

    CORRECCIONES PRIMER TEST

--- src/test/java/features/Griselda/Home.feature
+++ src/test/java/features/Griselda/Home.feature
@@ -2,10 +2,10 @@
 
 Característica: Estos test solo tienen click a paginas de la home de demoqa
 
-@prueba122 @TestCasillaVerificacion @elemenentos @checkbox
+ @TestCasillaVerificacion
 Escenario: Validar la selección de todos los checkbox de la seccion hogar
 Dado ingreso en elementos
 Cuando me dirijo a casilla de verificacion
-Y tildo todos
+Y tildo todos los checkbox de hogar
 Entonces  verifico que apareza el texto "You have selected :"
 
--- src/test/java/steps/Griselda/CheckBoxStepsG.java
+++ src/test/java/steps/Griselda/CheckBoxStepsG.java
@@ -12,13 +12,13 @@ public class CheckBoxStepsG {
     CheckBoxPageG objeto_check = new CheckBoxPageG(driver);
 
 
-    @Y("tildo todos")
-    public void tildoTodos() {
-        objeto_check.TildarTodos();
-    }
-
     @Entonces("verifico que apareza el texto {string}")
     public void verificoQueAparezaElTexto(String txt) {
         objeto_check.validarSeleccion(txt);
+    }
+
+    @Y("tildo todos los checkbox de hogar")
+    public void tildoTodosLosCheckboxDeHogar() {
+        objeto_check.TildarTodos();
     }
 }
