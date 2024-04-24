34b93b9f36d815a28c39163738bfeebcd029b0e2
pr-juan-k <djjuankalexis18@gmail.com>
2024-04-23 21:18:14 -0300

    Test en nuevo POM (martes 2 test de 6 pasos)

--- src/test/java/pages/BookPage.java
+++ src/test/java/pages/BookPage.java
@@ -13,19 +13,46 @@ public class BookPage {
     private final By btContra = By.xpath("//input[@id='password']");
     private final By btLog = By.xpath("//button[@id='login']");
     private final By btNewLog = By.xpath("//button[@id='newUser']");
+    private final By Pname = By.xpath("//input[@id='firstname']");
+    private final By Sname = By.xpath("//input[@id='lastname']");
+    private final By Uname = By.xpath("//input[@id='userName']");
+    private final By Ccontra = By.xpath("//input[@id='password']");
+    private final By noRobot = By.xpath("//*[@id='recaptcha-anchor']");
+    private final By btRegistrarse = By.xpath("//button[@id='register']");
+    private final By inicio = By.xpath("//img[@src='/images/Toolsqa.jpg']");
+
 
 
     public BookPage(WebDriver driver){
         this.mouse = new Click(driver);
         this.escribir = new Escribir(driver);
     }
-    public void clickLogin() throws Exception {
+    public void cierroTodo() throws Exception {
+        mouse.Clickear(inicio);
+    }
+    public void meRegistro() throws Exception {
+        mouse.Clickear(btRegistrarse);
+    }
+    public void meRegistro(String n1,String n2,String n3,String n4) throws Exception {
+        escribir.Escribir(Pname,n1);
+        escribir.Escribir(Sname,n2);
+        escribir.Escribir(Uname,n3);
+        escribir.Escribir(Ccontra,n4);
+
+    }
+    public void botonLogueo() throws Exception {
         mouse.Clickear(btLoginMenu);
+    }
+    public void clickNewLog() throws Exception {
+        mouse.Clickear(btNewLog);
+    }
+    public void clickLogin() throws Exception {
+        mouse.Clickear(btLog);
     }
     public void cargoForm(String n1,String n2) throws Exception {
         escribir.Escribir(btNombre,n1);
         escribir.Escribir(btContra,n2);
-        mouse.Clickear(btLog);
+
     }
 
 
--- src/test/java/steps/Formularios/BoockStep.java
+++ src/test/java/steps/Formularios/BoockStep.java
@@ -3,6 +3,7 @@ package steps.Formularios;
 import factory.WebDriverManager;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
 import org.openqa.selenium.WebDriver;
 import pages.BookPage;
@@ -15,13 +16,33 @@ public class BoockStep {
     @Dado("que me dirijo a biblioteca")
     public void queMeDirijoABiblioteca() throws Exception {
         inicio.clickBook();
+
     }
 
 
     @Y("voy a login y trato de ingresar como {string} y contraseña {string}")
     public void voyALoginYTratoDeIngresarComoYContrasena(String arg0, String arg1) throws Exception {
+        page.botonLogueo();
         page.cargoForm(arg0,arg1);
+
     }
 
 
+    @Cuando("trato de ingresar y no ingresa entonces voy a registrarme")
+    public void tratoDeIngresarYNoIngresaEntoncesVoyARegistrarme() throws Exception {
+        page.clickLogin();
+        page.clickNewLog();
+
+    }
+
+    @Y("cargo mis datos como {string} {string} {string} contraseña {string}")
+    public void cargoMisDatosComoContrasena(String arg0, String arg1, String arg2, String arg3) throws Exception {
+        page.meRegistro(arg0,arg1,arg2,arg3);
+    }
+
+    @Entonces("me registre y vuelvo al inicio")
+    public void meRegistreYVuelvoAlInicio() throws Exception {
+        page.meRegistro();
+        page.cierroTodo();
+    }
 }
